49980f08578fa0d66f22dc8304759438bbd30c3d
Diecx503 <80225651+Diecx503@users.noreply.github.com>
2021-11-21 10:23:59 -0600

    grrgfgdfnn
    frgbfgfh

--- Recursos/js/tareas.js
+++ Recursos/js/tareas.js
@@ -1,12 +1,11 @@
 //CREACION DE LOCALSTORAGE PARA CREDENCIALES
-
+let credenciales = {
+    nombre: "Ash Ketchum",
+    cuenta: "0987654321",
+    saldoInicial: 500.00
+}
 
 function guardardatos() {
-    let credenciales = {
-        nombre: "Ash Ketchum",
-        cuenta: "0987654321",
-        saldoInicial: 500.00
-    }
     //GUARDANDO OBJETO EN LOCALSTORAGE
     localStorage.setItem("credenciales", JSON.stringify(credenciales));
     //CONVIRTIENDO Y GUARDANDOLO EN UNA VARIABLE
@@ -14,8 +13,25 @@ function guardardatos() {
     //DANDOLE UBICACION AL OBJETO POR MEDIO DE UN ('ID')
     var usuario = document.getElementById('nombrecuenta');
     usuario.innerHTML = informacion.nombre + " " + informacion.cuenta;
-
     var saldo = document.getElementById('saldototal');
     saldo.innerHTML = "$" + informacion.saldoInicial;
 }
 guardardatos();
+
+
+//CREANDO EL LOCALSTORAGE DE TRANSACCIONES 
+let datos= [];
+datos =[{
+fecha:new Date().toLocaleString(),
+transaccion: "Deposito",
+monto: 500.00
+}];
+
+var fGuardarH= function guardarhistorial(){
+    localStorage.setItem("historial", JSON.stringify(datos));
+}
+
+function depositos(){
+    
+}
+
