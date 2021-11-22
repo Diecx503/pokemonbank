fc769cb6d5b597c493479c766fcfa46c3805b912
Diecx503 <80225651+Diecx503@users.noreply.github.com>
2021-11-21 22:06:41 -0600

    rtrtr
    rtrt

--- Recursos/js/tareas.js
+++ Recursos/js/tareas.js
@@ -12,29 +12,93 @@ function guardardatos() {
     let informacion = JSON.parse(localStorage.getItem("credenciales"));
     //DANDOLE UBICACION AL OBJETO POR MEDIO DE UN ('ID')
     var usuario = document.getElementById('nombrecuenta');
-    usuario.innerHTML = informacion.nombre + " " + informacion.cuenta;
     var saldo = document.getElementById('saldototal');
-    saldo.innerHTML = "$" + informacion.saldoInicial;
+    if (saldo == null) {
+        usuario.innerHTML = informacion.nombre + " " + informacion.cuenta;
+    } else {
+        usuario.innerHTML = informacion.nombre + " " + informacion.cuenta;
+        saldo.innerHTML = "$" + informacion.saldoInicial;
+    }
+
+
 }
-//guardardatos();
+guardardatos();
 
 
 //CREANDO EL LOCALSTORAGE DE TRANSACCIONES 
-let datos= [];
-datos =[{
-fecha:new Date().toLocaleString(),
-transaccion: "Deposito",
-monto: 500.00
+let datos = [];
+datos = [{
+    fecha: new Date().toLocaleString(),
+    transaccion: "Deposito",
+    monto: 500.00
+    
 }];
 
-var fGuardarH= function guardarhistorial(){
+function guardarhistorial() {
     localStorage.setItem("historial", JSON.stringify(datos));
 }
 
-function ingresos(){
-    let valor= document.getElementById('montodeposito').Value;
-    console.log(valor);
+function cargarHistorial() {
+    datosH = JSON.parse(localStorage.getItem("historial"));
+}
+cargarHistorial();
 
+function ingresos() {
+    vfecha = new Date().toLocaleString();
+    vtransaccion = "Deposito";
+    var vDeposito = document.getElementById('montodeposito').value;
+    nuevoDeposito = {
+        fecha: vfecha,
+        transaccion: vtransaccion,
+        monto: vDeposito
+    }
+    datos.push(nuevoDeposito);
+    guardarhistorial();
+}
+
+function retiro() {
+    vfecha = new Date().toLocaleString();
+    vtransaccion = "Retiro";
+    var vRetiro = document.getElementById('montoderetiro').value;
+    nuevoRetiro = {
+        fecha: vfecha,
+        transaccion: vtransaccion,
+        monto: vRetiro
+    }
+    datos.push(nuevoRetiro);
+    guardarhistorial();
+}
+
+function pago() {
+    vfecha = new Date().toLocaleString();
+    vtransaccion = "pago";
+    var vPago = document.getElementById('montodepago').value;
+    nuevopago = {
+        fecha: vfecha,
+        transaccion: vtransaccion,
+        monto: vPago
+    }
+    datos.push(nuevopago);
+    guardarhistorial();
 
 }
 
+var tabla = document.getElementById('tablaH');
+
+function tablaHistorial() {
+    let informacionTabla = JSON.parse(localStorage.getItem('historial'));
+    var tablaT = [];
+    let tablaHistorial = "<tr> <td>" + informacionTabla.vfecha + "</td>  <td>" + informacionTabla.vtransaccion + "</td> <td>" + informacionTabla.vmonto + "</td></tr>";
+    tablaT.push(tablaHistorial);
+    console.log(informacionTabla);
+localStorage.setItem('tablaTransacciones' ,tablaT);
+
+}
+tablaHistorial();
+
+function imprimirDatos(){
+var tablaInicio= localStorage.getItem('tablaTransacciones');
+document.getElementById("tablaH").innerHTML = tablaInicio;
+
+
+}
